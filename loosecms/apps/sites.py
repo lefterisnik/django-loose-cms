00457ac70c1eb4695d0419a79e2458078a4e9497
Lefteris Nikoltsios <lefteris.nikoltsios@gmail.com>
2015-09-11 22:12:03 +0300

    Add handler for DoesNotExist exception.

--- loosecms/apps/sites.py
+++ loosecms/apps/sites.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 from django.contrib.sites.apps import SitesConfig
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import ProgrammingError, OperationalError
 
 
@@ -15,6 +16,9 @@ class LooseCMSSiteConfig(SitesConfig):
             current_site = Site.objects.get_current()
         except (ProgrammingError, OperationalError) as e:
             return
+        except Site.DoesNotExist:
+            raise ObjectDoesNotExist("In settings.py you set wrong SITE_ID. Please set the SITE_ID to match "
+                                     "with these from database.")
 
         # TODO: think what we can do if values are deferrent. We must select which value we keep.
         # TODO: think what we can do about new policy of ckeditor from removing filebrowser. (todo image plugin)
