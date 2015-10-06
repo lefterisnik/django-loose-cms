8d95e014adb2d424b48dc0fcc83e6cbc5dc27f67
Lefteris Nikoltsios <lefteris.nikoltsios@gmail.com>
2015-10-06 13:25:21 +0300

    Handle language changes.

--- loosecms/middleware/locale.py
+++ loosecms/middleware/locale.py
@@ -5,18 +5,20 @@ from django.conf import settings
 from django.utils import translation
 from django.middleware.locale import LocaleMiddleware
 from django.core.urlresolvers import LocaleRegexURLResolver
-from django.utils.translation import get_language, get_language_from_path
+from django.utils.translation import get_language, get_language_from_path, get_language_from_request
 
 
 class SimpleLocaleMiddleware(LocaleMiddleware):
 
     def process_request(self, request):
-
         if self.is_language_prefix_patterns_used():
-            lang_code = (get_language_from_path(request.path_info) or
-                         settings.LANGUAGE_CODE)
-
-            translation.activate(lang_code)
+            user_language = get_language_from_request(request, True)
+            language = get_language_from_path(request.path_info)
+            if not language and user_language in settings.LANGUAGE_CODE:
+                language = settings.LANGUAGE_CODE
+            else:
+                language = user_language
+            translation.activate(language)
             request.LANGUAGE_CODE = translation.get_language()
 
 
@@ -25,7 +27,6 @@ class NoPrefixLocaleRegexURLResolver(LocaleRegexURLResolver):
     @property
     def regex(self):
         language_code = get_language()
-
         if language_code not in self._regex_dict:
             regex_compiled = (re.compile('', re.UNICODE)
                               if language_code == settings.LANGUAGE_CODE
