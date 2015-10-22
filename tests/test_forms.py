330ef981c6bf95dbc0050abb206f26ad10617b2c
Lefteris Nikoltsios <lefteris.nikoltsios@gmail.com>
2015-10-22 20:59:20 +0300

    Add tests to model and form fields.

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.conf import settings
-from django.contrib import admin
 from django.test import Client, TestCase
-from django.core.urlresolvers import reverse
 from loosecms.fields import *
-from .helpers import *
+from loosecms.plugin_modeladmin import PluginModelAdmin
+from loosecms.plugin_pool import plugin_pool
 
 
-'''class TestRTfield(models.Model):
-    title = models.CharField(max_length=100)
-    body = LoosecmsRichTextField()
+class Test(models.Model):
+    title = models.CharField(max_length=200)
 
     def __unicode__(self):
         return self.title
 
-admin.site.register(TestRTfield)'''
+
+class TestPlugin(PluginModelAdmin):
+    model = Test
+    name = 'Text'
+    plugin_cke = True
 
 
 class TestLoosecmsRTField(TestCase):
@@ -24,6 +26,12 @@ class TestLoosecmsRTField(TestCase):
         model_field = LoosecmsRichTextField()
         form_field = model_field.formfield()
         self.assertIsInstance(form_field, LoosecmsRichTextFormField)
+
+    def test_appending_plugins(self):
+        model_field = LoosecmsRichTextField()
+        plugin_pool.register_plugin(TestPlugin)
+        form_field = model_field.formfield()
+        self.assertIn('loosecms', form_field.extra_plugins)
 
 
 class TestLoosecmsTaggableManager(TestCase):
@@ -37,9 +45,19 @@ class TestLoosecmsTaggableManager(TestCase):
 class TestUploadFilePathField(TestCase):
 
     def test_get_prep_value(self):
-        model_field = UploadFilePathField()
+        model_field1 = UploadFilePathField()
+        model_field2 = UploadFilePathField()
+        model_field3 = UploadFilePathField()
+
+        filepath = '/%s/test_forms.py' % settings.MEDIA_ROOT
+        if model_field1.get_prep_value(filepath).startswith('/'):
+            self.fail('Model field return value which starts with /.')
+
+        filepath = None
+        self.assertEqual(model_field2.get_prep_value(filepath), None)
+
         filepath = settings.MEDIA_ROOT + 'test_forms.py'
-        self.assertNotIn(settings.MEDIA_ROOT, model_field.get_prep_value(filepath))
+        self.assertNotIn(settings.MEDIA_ROOT, model_field3.get_prep_value(filepath))
 
 
     def test_upload_filepath_path_form_field(self):
